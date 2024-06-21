57fdcb551aa7915def460656f9698b449aa1103c
ypur name <abc@gmail.com>
2024-06-22 05:03:26 +0700

    DinhTungLam index.html

--- portfolios/DinhTungLam/js/main.js
+++ portfolios/DinhTungLam/js/main.js
@@ -1,1 +1,35 @@
-console.log("Main.js loaded");
+let slideIndex = 1;
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function autoSlide() {
+    plusSlides(1);
+}
+
+setInterval(autoSlide, 3000);
