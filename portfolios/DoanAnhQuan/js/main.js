3f06769a57b285470a8b1ac7e3921e2c7c4a7ddf
quandoan21-legion <qdoan21.legion@gmail.com>
2024-06-22 10:07:35 +0700

    22/06/2024 them validate vaof portfolio cua DOAN ANH QUAN va add portfolio cua TONG QUAN MINH

--- portfolios/DoanAnhQuan/js/main.js
+++ portfolios/DoanAnhQuan/js/main.js
@@ -31,3 +31,50 @@ function dropDownMenu() {
         }
     }
 }
+function validateForm() {
+    document.getElementById("contact-form-validate-error").style.display = "none";
+    clearErrorMessage("subject");
+    clearErrorMessage("name");
+    clearErrorMessage("email");
+    clearErrorMessage("phone");
+    validateInputField("email")
+    validateInputField("phone")
+    validateInputField("name")
+    validateInputField("subject")
+}
+
+function clearErrorMessage(inputId) {
+    document.getElementById(inputId + "-error").style.display = "none";
+    document.getElementById(inputId).style.border = "#595959 solid 1px";
+}
+
+
+function validateInputField(inputId) {
+    let user = document.getElementById(inputId).value;
+    let user2 = document.getElementById(inputId);
+    let re
+    switch (inputId) {
+        case "email":
+            re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+            break
+        case "name":
+            re = /([a-zA-Z0-9_\s]+)/g;
+            break
+        case "subject":
+            re = /([a-zA-Z0-9_\s]+)/g;
+            break
+        case "phone":
+            re = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
+            break
+    }
+    if (re.test(user)) {
+        console.log("validated address");
+        return true;
+    } else {
+        user2.style.border = "red solid 3px";
+        document.getElementById("contact-form-validate-error").style.display = "block";
+        document.getElementById(inputId + "-error").style.display = "block";
+
+        return false;
+    }
+}
